600beacea375b19b40aa27779ecdd6761317887f
TijnFiks <fiks.tijn@gmail.com>
2023-06-28 20:39:02 +0200

    Todo list

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView
+from .models import ToDoList
+
+
+class ListListView(ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
