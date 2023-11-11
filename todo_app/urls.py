9b3b8f20a04d6742d047b22db3876986b2bed44c
TijnFiks <fiks.tijn@gmail.com>
2023-11-11 13:16:05 +0100

    Nieuwe templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ListListView.as_view(), name="index"),
+    path('', views.index),
+    path('list/', views.list),
+    path('new_list/', views.new_list),
+    path('new_item/', views.new_item),
+    path('delete_item/', views.delete_item),
+    path('item/', views.item),
+    path('delete_list/', views.delete_list),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,10 +2,72 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.views.generic import ListView
-from .models import ToDoList
+from .models import ToDoList, ToDoItem
 
 
-class ListListView(ListView):
-    model = ToDoList
-    template_name = "todo_app/index.html"
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+
+def index(request):
+    lists = ToDoList.objects.all()
+    return render(
+        request,
+        'index.html',
+        {
+            'lists': lists,
+        }
+    )
+
+def list(request):
+    list_id = request.GET['id']
+    list = ToDoList.objects.get(id=list_id)
+    items = ToDoItem.objects.filter(todo_list_id=list_id).order_by('completed')
+    return render(
+        request,
+        'list.html',
+        {
+            'items': items,
+            'list': list
+        }
+    )
+
+def new_item(request):
+    title = request.POST['item_title']
+    due_date = request.POST['due_date']
+    todo_list_id = int(request.POST['list_id'])
+    item = ToDoItem(title=title, due_date=due_date, todo_list_id=todo_list_id)
+    item.save()
+    return redirect(f'/list?id={todo_list_id}')
+
+def item(request):
+    id = request.GET['id']
+    employee = Employee.objects.get(id=id)
+    departments = Department.objects.all()
+    return render(
+        request,
+        'employee.html',
+        {
+            'employee': employee,
+            'departments': departments
+        }
+    )
+
+def delete_item(request):
+    id = int(request.POST['id'])
+    item = ToDoItem.objects.get(id=id)
+    item.delete()
+    return redirect(f'/list?id={item.todo_list_id}')
+
+
+def new_list(request):
+    title = request.POST['list_title']
+    list = ToDoList(title=title)
+    list.save()
+    return redirect(index)
+
+def delete_list(request):
+    id = int(request.POST['id'])
+    ToDoList.objects.get(id=id).delete()
+    return redirect(index)
+
+
